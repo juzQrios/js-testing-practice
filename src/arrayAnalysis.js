8739a8ec1a75cb46656ddeefa815ed6f8e1669fd
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-11-13 18:29:33 +0200

    add arrayAnalysis and its tests.

---
+++ src/arrayAnalysis.js
@@ -0,0 +1,14 @@
+const average = (array) => array.reduce((accum, item) => accum + item) / array.length;
+
+const max = (array) => Math.max(...array);
+
+const min = (array) => Math.min(...array);
+
+const arrayAnalysis = (array) => ({
+  average: average(array),
+  min: min(array),
+  max: max(array),
+  length: array.length
+});
+
+export default arrayAnalysis;
--- test/caesarCipher.test.js
+++ test/caesarCipher.test.js
@@ -18,11 +18,6 @@ test('convert full string with just spaces', () => {
 });
 
 test('convert full string with punctuation', () => {
-  expect(caesarCipher('If he had anything confidential to say', 7))
-    .toBe('Pm ol ohk hufaopun jvumpkluaphs av zhf');
-});
-
-test('convert full string with punctuation', () => {
   expect(caesarCipher('he wrote it in cipher, that is, by so changing the order of the letters of the alphabet.', 20))
     .toBe('by qliny cn ch wcjbyl, nbun cm, vs mi wbuhacha nby ilxyl iz nby fynnylm iz nby ufjbuvyn.');
 });
